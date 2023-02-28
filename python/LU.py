4ab544dd02439e576846aad7d43ceb73dc2eadc9
Chaithawat Saklang <90811655+HelloArtty@users.noreply.github.com>
2023-03-01 04:12:19 +0700

    dele ex #

--- python/LU.py
+++ python/LU.py
@@ -14,25 +14,6 @@ def lu_factorization(A):
     
     return L, U
 
-# def lu_factorization(A):
-    # n = len(A)
-    # L = np.zeros((n, n))
-    # U = np.zeros((n, n))
-    
-    # for j in range(n):
-    #     L[j, j] = 1.0
-    #     for i in range(j+1):
-    #         s = sum(U[k, j] * L[i, k] for k in range(i))
-    #         U[i, j] = A[i, j] - s
-    #     for i in range(j, n):
-    #         s = sum(U[k, j] * L[i, k] for k in range(j))
-    #         if U[j, j] == 0:
-    #             L[i, j] = A[i, j]
-    #         else:
-    #             L[i, j] = (A[i, j] - s) / U[j, j]
-    
-    # return L, U
-
 # Example usage
 A = np.array([[1, 3, 2], 
             [2, 1, 3], 
